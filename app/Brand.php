43a46b90737e5a27986d5660e8ddc4796a0470bc
francislagueu <lagson6@yahoo.fr>
2017-04-08 16:53:42 -0700

    Finished ORM implementation using Eloquent

--- app/Brand.php
+++ app/Brand.php
@@ -4,7 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Brand extends Model
+class Brand extends BaseModel
 {
-    //
+    protected $primaryKey = 'id';
+    protected $table = 'brands';
+    protected $fillable = array('name', 'created_at_ip', 'updated_at_ip');
 }
--- app/Http/Controllers/Front.php
+++ app/Http/Controllers/Front.php
@@ -2,50 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Brand;
+use App\Category;
+use App\Product;
 use Illuminate\Http\Request;
 
 class Front extends Controller
 {
-    //
+    var $brands;
+    var $categories;
+    var $products;
+    var $title;
+    var $description;
+
+    public function __construct()
+    {
+        $this->brands = Brand::all(array('name'));
+        $this->categories=Category::all(array('name'));
+        $this->products=Product::all(array('id','name','price'));
+    }
+
     public function index(){
-        return view('home', array('page'=>'home'));
+        return view('home', array('title'=> 'Welcome','page'=>'home', 'description'=>'' , 'brands'=>$this->brands, 'categories'=>$this->categories, 'products'=>$this->products));
     }
     public function products(){
-        return view('products', array('page'=>'products'));
+        return view('products', array( 'title'=>'Products Listing', 'page'=>'products', 'description'=>'' , 'brands'=>$this->brands, 'categories'=>$this->categories, 'products'=>$this->products));
     }
     public function product_details($id){
-        return view('product_details', array('page'=>'products'));
+        $product = Product::find($id);
+        return view('product_details', array( 'products'=>$product, 'title'=>$product->name, 'page'=>'products', 'description'=>'' , 'brands'=>$this->brands, 'categories'=>$this->categories, 'products'=>$this->products));
     }
-    public function product_categories(){
-        return view('products', array('page'=>'products'));
+    public function product_categories($name){
+        return view('products', array('page'=>'products', 'title'=>'Welcome', 'description'=>'' , 'brands'=>$this->brands, 'categories'=>$this->categories, 'products'=>$this->products));
     }
-    public function product_brand(){
-        return view('products', array('page'=>'products'));
+    public function product_brand($name, $category=null){
+        return view('products', array('page'=>'products', 'title'=>'Welcome', 'description'=>'' , 'brands'=>$this->brands, 'categories'=>$this->categories, 'products'=>$this->products));
     }
     public function blog(){
-        return view('blog', array('page'=>'blog'));
+        return view('blog', array('page'=>'blog','title'=>'Welcome', 'description'=>'' , 'brands'=>$this->brands, 'categories'=>$this->categories, 'products'=>$this->products));
     }
     public function blog_post($id){
-        return view('blog_post', array('page'=>'blog'));
+        return view('blog_post', array('page'=>'blog', 'title'=>'Welcome', 'description'=>'' , 'brands'=>$this->brands, 'categories'=>$this->categories, 'products'=>$this->products));
     }
     public function contact_us(){
-        return view('contact_us', array('page'=>'contact_us'));
+        return view('contact_us', array('page'=>'contact_us', 'title'=>'Welcome', 'description'=>''));
     }
     public function login(){
-        return view('login', array('page'=>'home'));
+        return view('login', array('page'=>'home', 'title'=>'Welcome', 'description'=>''));
     }
     public function logout(){
-        return view('logout', array('page'=>'home'));
+        return view('logout', array('page'=>'home', 'title'=>'Welcome', 'description'=>''));
     }
     public function cart(){
-        return view('cart', array('page'=>'home'));
+        return view('cart', array('page'=>'home', 'title'=>'Welcome', 'description'=>''));
     }
 
     public function checkout(){
-        return view('checkout', array('page'=>'home'));
+        return view('checkout', array('page'=>'home','title'=>'Welcome', 'description'=>''));
     }
     public function search($query){
-        return view('products', array('page'=>'products'));
+        return view('products', array('page'=>'products', 'title'=>'Welcome', 'description'=>''));
     }
 
 }
